73e9502f32e8443ed2b29055e33c5b95b815fb9e
juanmarin444 <30785213+Juanmarin444@users.noreply.github.com>
2021-10-11 18:43:13 -0700

    server config

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://javascript-arcade.netlify.app/'
+export const server = dev ? 'http://localhost:3000' : 'https://javascript-arcade.netlify.app'
