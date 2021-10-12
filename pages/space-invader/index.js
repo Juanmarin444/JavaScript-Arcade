64875e84a517dfe971be1d96225bd1d07ae30458
juanmarin444 <30785213+Juanmarin444@users.noreply.github.com>
2021-10-12 16:04:02 -0700

    Added Go back button

--- pages/space-invader/index.js
+++ pages/space-invader/index.js
@@ -1,18 +1,27 @@
 import Head from 'next/head'
 import Footer from '../../components/Footer'
 import styles from '../../styles/SpaceInvader.module.css'
+import Link from 'next/link'
 
 
 const SpaceInvader = ({props}) => {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Space Invader</title>
+        <meta name="description" content="Super fun space invader game" />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
       <main className={styles.main}>
-        <Head>
-          <title>Space Invader</title>
-          <meta name="description" content="Super fun space invader game" />
-          <link rel="icon" href="/favicon.svg" />
-        </Head>
         <h1>Space <span className={styles.accent}>Invaders</span></h1>
+        <p className={styles.description}>Coming Soon!</p>
+        <div className={styles.nav}>
+          <Link href='/' passHref>
+            <div className={styles.start}>
+              Go back
+            </div>
+          </Link>
+        </div>
       </main>
       <Footer />
     </div>
