926c67e4a555c603d987166debe2dc663398a199
juanmarin444 <30785213+Juanmarin444@users.noreply.github.com>
2021-11-04 19:10:42 -0700

    Game functionality added

--- pages/snake/index.js
+++ pages/snake/index.js
@@ -1,10 +1,33 @@
+// React dependancies
+import { useRef, useEffect, useState } from 'react'
+
+// Next dependancies
 import Head from 'next/head'
-import Footer from '../../components/Footer'
-import styles from '../../styles/Snake.module.css'
 import Link from 'next/link'
 
+// Components
+import Footer from '../../components/Footer'
+
+// Styles
+import styles from '../../styles/Snake.module.css'
+
+// Game Functionality
+import { startGame, controls } from './startGame'
+
+//Alert Messages
+import { Alert } from '../../components/alert';
 
 const Snake = ({props}) => {
+  const [isRunning, setIsRunning] = useState(false);
+  const squaresRef = useRef(null)
+  const scoreRef = useRef(null)
+
+  useEffect(() => {
+    if(isRunning) {
+      window.addEventListener('keyup', controls);
+    }
+  });
+
   return (
     <div className={styles.container}>
       <Head>
@@ -15,14 +38,19 @@ const Snake = ({props}) => {
       <main className={styles.main}>
         <h1>Snake<span className={styles.accent}></span></h1>
         <div className={styles.nav}>
+          <div className={styles.start} onClick={() => {setIsRunning(true); startGame(squaresRef, scoreRef);}}>Start/Restart</div>
           <Link href='/' passHref>
             <div className={styles.start}>
               Go back
             </div>
           </Link>
         </div>
-        <div className={styles.grid}>
+        <div className="score">Score: <span ref={scoreRef}>0</span></div>
+        <div className={styles.grid} ref={squaresRef}>
           {[...Array(400).keys()].map(index => <div key={index}></div>)}
+        </div>
+        <div className={styles.alertContainer}>
+          <Alert />
         </div>
       </main>
       <Footer />
