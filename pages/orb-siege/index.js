262a3fc0d1b9b7ad1e130ce4e5a378ec7c6462ef
juanmarin444 <30785213+Juanmarin444@users.noreply.github.com>
2021-12-17 18:14:16 -0800

    Orb Siege functionality

--- pages/orb-siege/index.js
+++ pages/orb-siege/index.js
@@ -2,15 +2,18 @@ import { useRef, useEffect, useState } from 'react'
 import Head from 'next/head'
 import Footer from '../../components/Footer'
 import styles from '../../styles/OrbSiege.module.css'
-import { startGame } from '../../services/orbSiege/startGame'
+import { startGame, shoot } from '../../services/orbSiege/startGame'
+// import gsap from 'gsap'
 
 const OrbSiege = () => {
   const canvasRef = useRef(null)
+  const scoreRef = useRef(null)
+  const uiModalRef = useRef(null)
+  const endScoreRef = useRef(null)
   const [isRunning, setIsRunning] = useState(false)
   useEffect(() => {
     if (isRunning) {
-      console.log("Chicken is now");
-      // startGame(canvasRef);
+      window.addEventListener('click', shoot)
     }
   })
   return (
@@ -20,20 +23,20 @@ const OrbSiege = () => {
         <meta name="description" content="Last as long as you can against the attach of the rainbow orbs" />
         <link rel="icon" href="/favicon.svg" />
       </Head>
-      <div className={styles.scoreContainer}><span>Score:</span><span id="score">0</span></div>
+      <div className={styles.scoreContainer}><span>Score:</span><span ref={scoreRef} id="score">0</span></div>
       <main className={styles.main}>
-        <div className={styles.uiModal} id="uiModal">
+        <div ref={uiModalRef} className={styles.uiModal} id="uiModal">
           <div className={styles.bg}>
             <h1>Orb <span className={styles.accent}>Siege</span></h1>
-            <h2 id="endScore">0</h2>
+            <h2 ref={endScoreRef} id="endScore">0</h2>
             <h3>Points</h3>
             <div className={styles.startBtn}>
-              <button onClick={() => {setIsRunning(true); startGame(canvasRef)}} id="startGameBtn">Start Game</button>
+              <button onClick={() => {setIsRunning(true); startGame(canvasRef, scoreRef, uiModalRef, endScoreRef)}} id="startGameBtn">Start Game</button>
             </div>
           </div>
           <Footer />
         </div>
-        <canvas ref={canvasRef} />
+        <canvas ref={canvasRef}></canvas>
       </main>
     </div>
   )
